fbea70d9f34272c40fd3958c904b896a15c1bc48
Eldrich Pereira <pereiraeldrich27@gmail.com>
2026-09-16 19:46:17 +0530

    Integrating search and log in navbar

--- popcorn/src/components/NavBar.tsx
+++ popcorn/src/components/NavBar.tsx
@@ -1,6 +1,7 @@
 
 import { useState } from "react";
 import { Logo } from "./Logo";
+import { Search } from "./Search";
 
 
 
@@ -9,8 +10,7 @@ export function NavBar(){
     return(
         <nav className="nav-bar">
             <Logo />
-            
-            
+            <Search />
       </nav>
     )
 }
--- popcorn/src/components/Search.tsx
+++ popcorn/src/components/Search.tsx
@@ -9,17 +9,16 @@ export function Search({movies = []}:SearchProps){
     const [query, setQuery] = useState("");
     return(
         <>
-        
-        <input
-                className="search"
-                type="text"
-                placeholder="Search movies..."
-                value={query}
-                onChange={(e) => setQuery(e.target.value)}
-        />
-        <p className="num-results">
-            Found <strong>{movies.length}</strong> results
-        </p>
+            <input
+                    className="search"
+                    type="text"
+                    placeholder="Search movies..."
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)}
+            />
+            <p className="num-results">
+                Found <strong>{movies.length}</strong> results
+            </p>
         </>
     )
 }
