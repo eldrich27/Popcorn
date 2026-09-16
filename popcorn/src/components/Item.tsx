a1d766aabcce9de26a2b46f7d8a0954356a9dd18
Eldrich Pereira <pereiraeldrich27@gmail.com>
2026-09-16 20:39:46 +0530

    Added Movie and watched movie list Item component

---
+++ popcorn/src/components/Item.tsx
@@ -0,0 +1,15 @@
+
+
+function Movie(){
+    return(
+        <h1>Movie Item</h1>
+    )
+}
+
+function WatchedMovie(){
+    return(
+        <h1>Watched Movie Item</h1>
+    )
+}
+
+export {Movie, WatchedMovie}
