2cc765691d2471f069bb795398b09c78ac0dce36
Eldrich Pereira <pereiraeldrich27@gmail.com>
2026-09-18 11:49:12 +0530

    Displaying rating on the app display

--- popcorn/src/main.tsx
+++ popcorn/src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+// import App from './App.tsx'
+import { Rating } from './components/Rating/Rating'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <Rating></Rating>
   </StrictMode>,
 )
